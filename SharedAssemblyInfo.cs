c0546dd6fe1fa2ac76aa45e0f75df7dddddaba4b
gre4bee <gre4bee@gmail.com>
2021-07-03 11:49:02 +0800

    Update Assembly version
    --update assembly to 4.6.8.2

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.6.8.1")]
-[assembly: AssemblyFileVersion("4.6.8.1")]
+[assembly: AssemblyVersion("4.6.8.2")]
+[assembly: AssemblyFileVersion("4.6.8.2")]
